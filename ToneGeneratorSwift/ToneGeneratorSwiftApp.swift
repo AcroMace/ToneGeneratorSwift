e8d8fa04834a6fe1ab5009505270b9d0d34d50c3
Andy Cho <hello@andycho.ca>
2022-10-10 21:17:43 +0900

    Initial Commit

---
+++ ToneGeneratorSwift/ToneGeneratorSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneGeneratorSwiftApp.swift
+//  ToneGeneratorSwift
+//
+//  Created by Andy Cho on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ToneGeneratorSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
